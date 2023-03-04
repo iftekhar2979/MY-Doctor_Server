8b61d19f050ef7536ece2dad9f53cb2dc2246158
Iftekhar <97031620+iftekhar2979@users.noreply.github.com>
2023-03-05 02:03:09 +0600

    booking functionality added

--- Router/router.js
+++ Router/router.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const appoinmentModel = require('../Model/appointmentModel');
+const bookingModel = require('../Model/bookingModel');
 const router = new express.Router();
 
+router.get('/', async (req, res) => {
+  res.send('HI I aam the server');
+});
+router.get('/appointments', async (req, res) => {
+  try {
+    const appointments = await appoinmentModel.find({});
+    res.send(appointments);
+  } catch (error) {
+    res.status(404).send('Sorrry Server is Getting Error');
+  }
+});
+router.post('/booking', async (req, res) => {
+  const bookingProperty = req.body;
+  try {
+    const booking = new bookingModel({...bookingProperty});
+    const bookingAppointment=await booking.save()
+    return res.status(201).send(bookingAppointment)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).send({error:"data Not found" ,message:error.message})
+  }
+});
 
-router.get("/",async(req,res)=>{
-    res.send("HI I aam the server")
-})
-router.get("/appointments",async(req,res)=>{
-    try{
-        const appointments=await appoinmentModel.find({})
-        res.send(appointments)
-    }catch(error){
-       res.status(404).send("Sorrry Server is Getting Error")
-    }
-})
-
-module.exports=router
+module.exports = router;
--- index.js
+++ index.js
@@ -8,9 +8,6 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 app.use(router)
-
-
-
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
